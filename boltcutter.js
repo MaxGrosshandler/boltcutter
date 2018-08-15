f8177bab670088b4e0a1483fa709639c6aebc460
Max Grosshandler <maxgrosshandler@gmail.com>
2018-08-15 11:57:01 -0500

    Created just enough schema to actually run it through cf push

--- boltcutter.js
+++ boltcutter.js
@@ -15,29 +15,31 @@ bot.on("ready", () => {
   console.log("Ready!");
 });
 
-bot.registerCommand("ping", (msg) => {
+bot.registerCommand("ping", async msg => {
   await msg.channel.createMessage("Pong!").then(m => {
     let time = m.timestamp - msg.timestamp;
     return m.edit(`Pong! **${time}**ms`);
   }),
-   {
-  description: "Pong!",
-  fullDescription:
-    "This command gives the bot's latency!"
-}
+    {
+      description: "Pong!",
+      fullDescription: "This command gives the bot's latency!"
+    };
 });
-bot.registerCommand("echo", (msg, args) => { 
-  if(args.length === 0) {
+bot.registerCommand(
+  "echo",
+  (msg, args) => {
+    if (args.length === 0) {
       return "Invalid input";
+    }
+    let text = args.join(" ");
+    return text;
+  },
+  {
+    description: "Make the bot say something",
+    fullDescription: "The bot will echo whatever is after the command label.",
+    usage: "<text>"
   }
-  let text = args.join(" ");
-  return text; 
-}, {
-  description: "Make the bot say something",
-  fullDescription: "The bot will echo whatever is after the command label.",
-  usage: "<text>"
-});
-
+);
 
 app.use(express.static(__dirname + "/public"));
 app.listen(process.env.PORT || 4000);
